376240b2374e6cbdc2d022766d1340686e20ed91
Rodolpho Porto <esx_rporto@veloecorp.com.br>
2024-08-14 17:48:12 -0300

    Ensure LoginPage presents error if password is invalid.

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -12,17 +12,21 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 void main() {
   LoginPresenter presenter;
   StreamController<String> emailErrorController;
+  StreamController<String> passwordErrorController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
     emailErrorController = StreamController<String>();
+    passwordErrorController = StreamController<String>();
     when(presenter.emailErrorStream).thenAnswer((_) => emailErrorController.stream);
+    when(presenter.passwordErrorStream).thenAnswer((_) => passwordErrorController.stream);
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
   
   tearDown(() {
     emailErrorController.close();
+    passwordErrorController.close();
   });
 
   testWidgets('Should load with correct initial state', (WidgetTester tester) async {
@@ -91,4 +95,14 @@ void main() {
         findsOneWidget
     );
   });
+
+  testWidgets('Should present error with password is invalid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add('any error');
+    await tester.pump();
+
+    expect(find.text('any error'), findsOneWidget);
+  });
+
 }
